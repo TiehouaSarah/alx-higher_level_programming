a1329fde03dde6f035bba45977b241544ed71c01
Tiehoua Sarah <sarahjc225@gmail.com>
2023-03-28 04:07:56 -0700

    task revision

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     y = 0
     try:
-        y = "{:d}".format(a // b)
+        y = float("{:d}".format(a // b))
     except (ValueError, ZeroDivisionError):
         y = None
     finally:
